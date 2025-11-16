3636f698ccee9792b561c93b1ce17e230f3ed252
Vanshika Dakour <dakourvanshikadakour@gmail.com>
2025-11-16 13:25:17 +0530

    updating

--- backend/config/Cloudinary.js
+++ backend/config/Cloudinary.js
@@ -1,9 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
